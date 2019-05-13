51ff8f629472e046270310fad3278c1a8c6dc47b
Invité <guest-uh86bx@igtp5-pc04.isim.intra>
2019-05-13 11:54:36 +0200

    Modification des views pour ne pas charger trop de html

--- application/views/Register.php
+++ application/views/Register.php
@@ -1,17 +1,3 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html>
-<head>
-	<title>Inscription</title>
-  	<meta charset="utf-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="/ProjetWeb/application/assets/Register.css">
-	<link rel="stylesheet" href="/ProjetWeb/application/assets/General.css">
-</head>
 <body>
 	<div class ="container">
 		<div class ="row">

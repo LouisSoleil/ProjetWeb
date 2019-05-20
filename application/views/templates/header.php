4b15e100d03f5a73ab5acd626e00298af2e99aa8
Louis <louis.soleilhavoup@hotmail.com>
2019-05-20 17:21:18 +0200

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="<?php echo base_url('assets/General.css');?>">
+  <link rel="stylesheet" href="<?php echo base_url('application/assets/General.css');?>">
 
   <?php $CI =& get_instance();
   $this->load->model('My_Cookie');
